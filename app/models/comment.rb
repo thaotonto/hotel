0acc9784e7fa817f5cf172d65177385c4424cc37
thaotonto <ots.hunter@gmail.com>
2018-11-06 22:26:48 +0700

    reply

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :review
-  has_many :replies
+  has_many :feedbacks, class_name: "Replie", dependent: :destroy
 
   validates :content, presence: true
   scope :order_comment, ->{order created_at: :desc}
